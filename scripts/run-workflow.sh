df342ec30496d417810fb2312a348ac2fc729224
Rafael Ferreira da Silva <silvarf@ornl.gov>
2023-01-06 18:33:25 +0000

    adding config files

---
+++ scripts/run-workflow.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# reorganize work dir
+cd wfbench-workflow
+mkdir data
+mv *.txt data
+cp ../cpu-benchmark .
+
+# generate pegasus YAML workflow
+export PYTHONPATH=$PYTHONPATH:/usr/lib/python3.6/dist-packages
+python3 pegasus-workflow.py
+
+pegasus-plan --dir work --cleanup none --output-site local --submit `ls *.yml`
+sleep 30
+
+PID_FILE=`ls work/cc/pegasus/*/run0001/*.pid`
+
+echo "Waiting for workflow execution to complete..."
+
+while [[ -f $PID_FILE ]]
+do
+  sleep 30
+done
+
+echo "Workflow execution completed."
+
---
+++ setup/install-submit-node.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+CONDOR_HOST=$1
+
+# install HTCondor
+apt-get update && apt-get install -y curl
+curl -fsSL https://get.htcondor.org | sudo /bin/bash -s -- --no-dry-run
+rm /etc/condor/config.d/00-minicondor
+cat <<EOT>> /etc/condor/config.d/00-minicondor
+use ROLE: CentralManager
+use ROLE: Submit
+
+BIND_ALL_INTERFACES = True
+CONDOR_HOST = $CONDOR_HOST
+
+SEC_DEFAULT_AUTHENTICATION = OPTIONAL
+SEC_DEFAULT_AUTHENTICATION_METHODS = CLAIMTOBE
+
+SCHEDD_INTERVAL = 5
+NEGOTIATOR_INTERVAL = 2
+NEGOTIATOR_CYCLE_DELAY = 5
+STARTER_UPDATE_INTERVAL = 5
+SHADOW_QUEUE_UPDATE_INTERVAL = 10
+UPDATE_INTERVAL = 5
+RUNBENCHMARKS = 0
+
+HOSTALLOW_WRITE = *
+HOSTALLOW_READ = *
+ALLOW_WRITE = *
+ALLOW_READ = *
+ALLOW_NEGOTIATOR = *
+ALLOW_DAEMON = *
+ALLOW_COLLECTOR = *
+NUM_CPUS = 16
+EOT
+
+systemctl restart condor
+
+# install Pegasus
+wget -O - http://download.pegasus.isi.edu/pegasus/gpg.txt | sudo apt-key add -
+echo 'deb [arch=amd64] http://download.pegasus.isi.edu/pegasus/ubuntu bionic main' | sudo tee /etc/apt/sources.list.d/pegasus.list
+apt-get update
+apt-get install -y pegasus
+
+# install WfCommons
+cd $HOME
+git clone https://github.com/wfcommons/wfcommons
+cd wfcommons
+pip install -e .
+cd wfcommons/wfbench
+g++ -o cpu_benchmark cpu_benchmark.cpp
+cp cpu_benchmark $HOME
+
+# configure environment
+cd $HOME
+cp calibration_experiments/scripts/exp.py .
+cp calibration_experiments/scripts/run-workflow.sh .
+
