c0cb96e15cd62408040b45d02d9836e25d5649cc
Rafael Ferreira da Silva <silvarf@ornl.gov>
2023-03-24 09:34:22 -0400

    Update create-chain-workflow.py

--- scripts/create-chain-workflow.py
+++ scripts/create-chain-workflow.py
@@ -47,7 +47,7 @@ def get_arguments(i):
     arguments = []
 
     if args.command == "Sleep":
-        arguments.append(args.seconds)
+        arguments.append(f"\"{args.seconds}\"")
     elif args.command == "WfBench":
         arguments = [
             "chain_" + str(i).zfill(8),
