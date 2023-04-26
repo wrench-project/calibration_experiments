ddfe9fe9038f4f159fdf17fe35df2c35ecfe696d
Henri Casanova <henric@hawaii.edu>
2023-04-26 11:04:29 -1000

    Script improvement

--- scripts/run_all_experiments.sh
+++ scripts/run_all_experiments.sh
@@ -1,18 +1,25 @@
 #!/bin/bash 
 
-if [[ $# -ne 1 ]] ; then
-    echo "Usage: $0 <output dir path>"
+if [[ $# -ne 4 ]] ; then
+    echo "Usage: $0 <output dir path> <num compute nodes> <architecture> <num trials>"
+    echo " You may want to edit this script to change cpu work, data footprint, and/or workflow size values"
     exit 1
 fi
+
+OUTPUT_DIR=$1
+NUM_COMPUTE_NODES=$2
+ARCHITECTURE=$3
+NUM_TRIALS=$4
+
 
 # Real workflows
 real_workflows="seismology montage genome soykb cycles epigenomics bwa"
 for real_workflow in $real_workflows; do
-        ./run_experiments.py  -a haswell -w $real_workflow -n 1 -t 1 -c 0 500 5000 10000 20000 -f 0.6 -d 0 100000000 1000000000 10000000000 -o $1 -s 1.0 2.0 5.0 10.0
+        ./run_experiments.py  -a "${ARCHITECTURE}" -w "${real_workflow}" -n "${NUM_COMPUTE_NODES}" -t "${NUM_TRIALS}" -c 0 500 5000 10000 20000 -f 0.6 -d 0 100000000 1000000000 10000000000 -o ${OUTPUT_DIR} -s 1.0 2.0 5.0 10.0
 done
 
 # Chain
-./run_experiments.py  -a haswell -w chain -n 1 -t 1 -c 0 500 5000 10000 20000 -f 0.6 -d 0 100000000 1000000000 10000000000 -o $1 -S 1 5 10
+./run_experiments.py  -a "${ARCHITECTURE}" -w chain -n "${NUM_COMPUTE_NODES}" -t "${NUM_TRIALS}" -c 0 500 5000 10000 20000 -f 0.6 -d 0 100000000 1000000000 10000000000 -o ${OUTPUT_DIR} -S 1 5 10
 
 # Forkjoin
-./run_experiments.py  -a haswell -w forkjoin -n 1 -t 1 -c 0 500 5000 10000 20000 -f 0.6 -d 0 100000000 1000000000 10000000000 -o $1 -S 10 18 25 34 50 66 100 130 200
+./run_experiments.py  -a "${ARCHITECTURE}" -w forkjoin -n ${NUM_COMPUTE_NODES} -t "${NUM_TRIALS}" -c 0 500 5000 10000 20000 -f 0.6 -d 0 100000000 1000000000 10000000000 -o ${OUTPUT_DIR} -S 10 18 25 34 50 66 100 130 200
