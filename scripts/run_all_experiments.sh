81bd86da1a64f5b32471843d43581297e5a790ef
Henri Casanova <henric@hawaii.edu>
2024-09-06 15:07:30 -1000

    script fix

--- scripts/run_all_experiments.sh
+++ scripts/run_all_experiments.sh
@@ -48,17 +48,17 @@ CPU_FRACTION=1.0
 /bin/rm -rf /home/cc/wfbench-workflow/
 
 # Chain
-./run_experiments.py  -a "${ARCHITECTURE}" -w chain -n "${NUM_COMPUTE_NODES}" -t "${NUM_TRIALS}" -c 0 500 1000 5000 50000 -f "${CPU_FRACTION}" -d 0 150000000 1500000000 15000000000 -o ${OUTPUT_DIR} -S 2 5 10
+./run_experiments.py  -a "${ARCHITECTURE}" -w chain -n "${NUM_COMPUTE_NODES}" -t "${NUM_TRIALS}" -c 0 500 1000 5000 50000 -f "${CPU_FRACTION}" -d 0 150000000 1500000000 -o ${OUTPUT_DIR} -S 10 25 50 100
 
 # Forkjoin
-./run_experiments.py  -a "${ARCHITECTURE}" -w forkjoin -n ${NUM_COMPUTE_NODES} -t "${NUM_TRIALS}" -c 0 500 1000 5000 50000 -f "${CPU_FRACTION}" -d 0 150000000 1500000000 15000000000 -o ${OUTPUT_DIR} -S 10 18 25 34 50 66 100 130 200
+./run_experiments.py  -a "${ARCHITECTURE}" -w forkjoin -n ${NUM_COMPUTE_NODES} -t "${NUM_TRIALS}" -c 0 500 1000 5000 50000 -f "${CPU_FRACTION}" -d 0 150000000 1500000000 -o ${OUTPUT_DIR} -S 10 25 50 100
 
 # Real workflows
 #real_workflows="seismology montage genome soykb cycles epigenomics bwa"
 real_workflows="seismology"
 
 for real_workflow in $real_workflows; do
-        ./run_experiments.py  -a "${ARCHITECTURE}" -w "${real_workflow}" -n "${NUM_COMPUTE_NODES}" -t "${NUM_TRIALS}" -c 0 500 1000 5000 50000 -f "${CPU_FRACTION}" -d 0 150000000 1500000000 15000000000 -o ${OUTPUT_DIR} -s 1.0 2.0 5.0 10.0
+        ./run_experiments.py  -a "${ARCHITECTURE}" -w "${real_workflow}" -n "${NUM_COMPUTE_NODES}" -t "${NUM_TRIALS}" -c 0 500 1000 5000 50000 -f "${CPU_FRACTION}" -d 0 150000000 1500000000 -o ${OUTPUT_DIR} -s 1.0 2.0 5.0 10.0
 done
 
 
