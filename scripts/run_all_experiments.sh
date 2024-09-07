0868e7f91f92659121e1bf285591b2472759098d
Henri Casanova <henric@hawaii.edu>
2024-09-07 05:43:51 -1000

    script change

--- scripts/run_all_experiments.sh
+++ scripts/run_all_experiments.sh
@@ -48,10 +48,10 @@ CPU_FRACTION=1.0
 /bin/rm -rf /home/cc/wfbench-workflow/
 
 # Chain
-./run_experiments.py  -a "${ARCHITECTURE}" -w chain -n "${NUM_COMPUTE_NODES}" -t "${NUM_TRIALS}" -c 0 500 1000 5000 50000 -f "${CPU_FRACTION}" -d 0 150000000 1500000000 -o ${OUTPUT_DIR} -S 10 25 50 100
+./run_experiments.py  -a "${ARCHITECTURE}" -w chain -n "${NUM_COMPUTE_NODES}" -t "${NUM_TRIALS}" -c 0 500 1000 5000 50000 -f "${CPU_FRACTION}" -d 0 150000000 1500000000 -o ${OUTPUT_DIR} -S 10 25 50 
 
 # Forkjoin
-./run_experiments.py  -a "${ARCHITECTURE}" -w forkjoin -n ${NUM_COMPUTE_NODES} -t "${NUM_TRIALS}" -c 0 500 1000 5000 50000 -f "${CPU_FRACTION}" -d 0 150000000 1500000000 -o ${OUTPUT_DIR} -S 10 25 50 100
+./run_experiments.py  -a "${ARCHITECTURE}" -w forkjoin -n ${NUM_COMPUTE_NODES} -t "${NUM_TRIALS}" -c 0 500 1000 5000 50000 -f "${CPU_FRACTION}" -d 0 150000000 1500000000 -o ${OUTPUT_DIR} -S 10 25 50 
 
 # Real workflows
 #real_workflows="seismology montage genome soykb cycles epigenomics bwa"
